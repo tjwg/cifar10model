1fee97287fa6631c4a504fb5eac8301d7fc7ddd0
tjwg <garethmeg2005@gmail.com>
2025-02-23 02:27:59 +0800

    Update main.py

--- dense_net_model/main.py
+++ dense_net_model/main.py
@@ -6,7 +6,8 @@ from PIL import Image, UnidentifiedImageError
 import os
 
 # Load model safely
-loaded_model = tf.keras.models.load_model('best_densenet121_model.h5')
+MODEL_PATH = 'best_densenet_model.h5'
+loaded_model = tf.keras.models.load_model(MODEL_PATH)
 #loaded_model = tf.keras.models.load_model(MODEL_PATH, compile=False)
 #loaded_model.build((None, 32, 32, 3))  # Adjust this if your model expects a different size
 
