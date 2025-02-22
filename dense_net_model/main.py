8de4b0bf041750bff52dbf72b8b523e7304c99ce
tjwg <garethmeg2005@gmail.com>
2025-02-23 02:32:20 +0800

    Update main.py

--- dense_net_model/main.py
+++ dense_net_model/main.py
@@ -7,8 +7,7 @@ import os
 
 # Load model safely
 MODEL_PATH = 'best_densenet_model.h5'
-loaded_model = tf.keras.models.load_model(MODEL_PATH)
-#loaded_model = tf.keras.models.load_model(MODEL_PATH, compile=False)
+loaded_model = tf.keras.models.load_model(MODEL_PATH, compile=False)
 #loaded_model.build((None, 32, 32, 3))  # Adjust this if your model expects a different size
 
 if not os.path.exists(MODEL_PATH):
